caf247ef59b6244912e6c24abdc991f12718019d
yesprasad <y.eshwarprasad@gmail.com>
2018-09-01 18:51:53 +0530

    index.js changes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+//components
+import Home from './components/home';
+import Books from './components/books';
+import Profile from './components/profile';
 
 const App = () => {
     return(
-        <div>
-            Welcome to routing concepts
-        </div>
+        <BrowserRouter>
+            <div>
+                <Route path='/' exact component={Home}/>
+                <Route path='/books' component={Books}/>
+                <Route path='/profile' component={Profile}/>
+            </div>
+        </BrowserRouter>
+        
     );
 };
 
